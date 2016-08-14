5baa07ca3e2bd0032843cfbed8e861b79b4a0c47
Nathan Bishop <nbish11@hotmail.com>
2016-08-15 04:11:51 +1000

    Fix problem with modules not loading

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 module.exports = {
-    environments: require('./environments');
+    environments: {
+        core: {
+            globals: {
+                "$": false,
+                "$$": false,
+                "Browser": false,
+                "Chain": false,
+                "Class": false,
+                "Cookie": false,
+                "Core": false,
+                "Document": false,
+                "DomReady": false,
+                "DOMEvent": false,
+                "DOMReady": false,
+                "Element": false,
+                "Elements": false,
+                "Event": false,
+                "Events": false,
+                "Fx": false,
+                "Hash": false,
+                "IFrame": false,
+                "instanceOf": false,
+                "MooTools": false,
+                "Native": false,
+                "Options": false,
+                "Request": false,
+                "Selectors": false,
+                "Slick": false,
+                "Type": false,
+                "typeOf": false,
+                "Window": false
+            }
+        },
+
+        more: {
+            globals: {
+                "Asset": false,
+                "Color": false,
+                "Drag": false,
+                "Group": false,
+                "HtmlTable": false,
+                "IframeShim": false,
+                "InputValidator": false,
+                "Keyboard": false,
+                "Locale": false,
+                "Mask": false,
+                "OverText": false,
+                "Scroller": false,
+                "Slider": false,
+                "Sortables": false,
+                "Spinner": false,
+                "Swiff": false,
+                "Table": false,
+                "Tips": false,
+                "URI": false
+            }
+        }
+    }
 };
--- src/environments/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    core: require('./core.js'),
-    more: require('./more.js')
-};
